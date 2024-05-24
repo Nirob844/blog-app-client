d0009cbfacf9345bad4dc17f70ed7b0d72dbb92a
Nirob Hasan <nirob427417@gmail.com>
2024-05-24 11:31:35 +0600

    posts

--- src/pages/Post/PostCard.js
+++ src/pages/Post/PostCard.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import './PostCard.css'
+import React from "react";
+import "./PostCard.css";
 
-const PostCard = () => {
-    return (
-        <div className='border w-96 m-4 p-4 rounded-md border-black-400'>
-            <h3 className='text-lg font-bold'>This is post Title</h3>
-            <div className='text-state-500 text-sm text-gray-700 flex justify-between py-2'>
-                <p>Author Name</p>
-                <p>Date: </p>
-            </div>
-            <p className='text-sm'>This is Content</p>
-        </div>
-    );
+const PostCard = ({ post }) => {
+  console.log(post);
+  return (
+    <div className="border w-96 m-4 p-4 rounded-md border-black-400">
+      <h3 className="text-lg font-bold">This is post Title</h3>
+      <div className="text-state-500 text-sm text-gray-700 flex justify-between py-2">
+        <p>Author Name</p>
+        <p>Date: </p>
+      </div>
+      <p className="text-sm">This is Content</p>
+    </div>
+  );
 };
 
 export default PostCard;
--- src/pages/Post/Posts.js
+++ src/pages/Post/Posts.js
@@ -1,14 +1,37 @@
-import React from 'react';
-import PostCard from './PostCard';
+import { gql, useQuery } from "@apollo/client";
+import React from "react";
+import PostCard from "./PostCard";
+
+const GET_POSTS = gql`
+  query Posts {
+    posts {
+      title
+      content
+      author {
+        name
+        email
+      }
+    }
+  }
+`;
 
 const Posts = () => {
-    return (
-        <div>
-            <h1 className='text-center font-bold text-5xl my-4 pb-4'>Posts</h1>
-            <hr />
-            <PostCard></PostCard>
-        </div>
-    );
+  const { loading, error, data } = useQuery(GET_POSTS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+
+  return (
+    <div>
+      <h1 className="text-center font-bold text-5xl my-4 pb-4">Posts</h1>
+      <hr />
+      <div className="flex flex-wrap">
+        {data.posts.map((post) => (
+          <PostCard post={post} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Posts;
