00002a93336a0151b49e482d5e7e784de909b4c2
Nirob Hasan <nirob427417@gmail.com>
2024-05-24 11:55:42 +0600

    signup

--- src/pages/Signin/Signin.js
+++ src/pages/Signin/Signin.js
@@ -1,28 +1,71 @@
-import React from 'react';
+import { gql, useMutation } from "@apollo/client";
+import React, { useEffect, useState } from "react";
+import "./Signup.css";
+
+const SIGNUP = gql`
+  mutation Reg(
+    $name: String!
+    $email: String!
+    $password: String!
+    $bio: String
+  ) {
+    signup(name: $name, email: $email, password: $password, bio: $bio) {
+      userError
+      token
+    }
+  }
+`;
 
 const Signin = () => {
-    const handleRegister = (e) => {
-        e.preventDefault();
-        const data = {
-            email: e.target.email.value,
-            password: e.target.password.value,
-        }
-        console.log("data: ", data)
+  const [userError, setUserError] = useState(null);
+  const [signup, { data, loading, error }] = useMutation(SIGNUP);
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+    const data = {
+      name: e.target.name.value,
+      email: e.target.email.value,
+      password: e.target.password.value,
+      bio: e.target.bio.value,
+    };
+    console.log("data: ", data);
+
+    signup({
+      variables: data,
+    });
+  };
+
+  useEffect(() => {
+    if (data && data.signup.token) {
+      console.log("token", data.signup.token);
+      localStorage.setItem("token", data.signup.token);
     }
+    if (data && data.signup.userError) {
+      setUserError(data.signup.userError);
+    }
+  }, [data]);
 
-    return (
-        <div className="form">
-            <form onSubmit={handleRegister}>
+  if (loading) return "Submitting...";
+  if (error) return `Submission error! ${error.message}`;
 
-                <label htmlFor="">Your Email</label>
-                <input name="email" type="email" />
-                <label htmlFor="">Your Password</label>
-                <input name="password" type="password" />
+  console.log("data: ", data);
+  console.log(userError);
 
-                <button className='rounded-full p-2 bg-white text-black'>Login</button>
-            </form>
-        </div>
-    );
+  return (
+    <div className="form">
+      <form onSubmit={handleRegister}>
+        <label htmlFor="">Your Email</label>
+        <input name="email" type="email" />
+        <label htmlFor="">Your Password</label>
+        <input name="password" type="password" />
+
+        <button type="submit" className="rounded-full p-2 bg-white text-black">
+          Login
+        </button>
+      </form>
+      {userError && <div className="error">{userError}</div>}
+    </div>
+  );
 };
 
 export default Signin;
